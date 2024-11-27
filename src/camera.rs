5274626724e03e4dbc0e5ab6db3e26ef26481894
Kellen Watt <kellen@wattsoft.dev>
2024-11-26 22:38:12 -0600

    Yeah, this isn't working

--- src/camera.rs
+++ src/camera.rs
@@ -3,7 +3,7 @@
 
 use libcamera::{
     camera::{self, CameraConfigurationStatus},
-    // camera_manager as mgr,
+    camera_manager as mgr,
     framebuffer::AsFrameBuffer,
     framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
     framebuffer_map::MemoryMappedFrameBuffer,
@@ -15,12 +15,14 @@ use libcamera::{
 };
 
 // use crate::signal_thread::SignaledThread;
-use crate::thread::WorkerThread;
+// use crate::thread::WorkerThread;
 
-// use std::sync::mpsc;
+use std::sync::mpsc;
 use std::sync::{RwLock, Mutex, Arc};
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::atomic::{AtomicU64, AtomicBool, Ordering};
+use std::thread::{self, JoinHandle};
 
+use std::rc::Rc;
 
 pub enum PixelFormat {
     RGB,
@@ -93,21 +95,7 @@ impl Frame {
     }
 }
 
-struct Cam(camera::ActiveCamera<'static>);
-
-impl std::ops::Deref for Cam {
-    type Target = camera::ActiveCamera<'static>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl std::ops::DerefMut for Cam {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
+struct Cam<'a>(camera::Camera<'a>);
 
 // We have to do this because the cam is functionally owned by the thread, but 
 // we're keeping it around basically just for start and stop
@@ -121,161 +109,256 @@ impl std::ops::DerefMut for Cam {
 // between the threads, once started. The final compiler pinch point is that libcamera_request_t
 // isn't Send, except Request is, so it should be completely fine. Also, the two threads should
 // never conflict on requests, since buffers won't be user-specified.
-unsafe impl Send for Cam {}
+unsafe impl Send for Cam<'_> {}
 
+struct Cfg(camera::CameraConfiguration);
+unsafe impl Send for Cfg {}
+
+struct Mgr(mgr::CameraManager);
+unsafe impl Send for Mgr {}
+
+// This is not true in general! It's only true in this case where a great deal of care has 
+// gone into making sure its safe enough. This is purely to "trick" the compiler into accepting
+// safe-ish code, and is very much not the case in any other code other than when we're using 
+// a mutex to lock the Mgr down during writes.
+unsafe impl Sync for Mgr {}
+
+static mut mgr_instanced: AtomicBool = AtomicBool::new(false);
+
+pub struct CameraManager {
+    // This absolute abomination of a type exists so that a reference to the manager can 
+    // exist within each thread, but it's still safe to access the manager during the
+    // initial allocations.
+    //
+    // To do this, the outer Arc is cloned with the Mutex to share with each thread.
+    // Then the Mutex locks down the manager, so it can create the camera list safely and
+    // allocate the camera. Specifically, all of the allocations are made using a clone
+    // of the inner Arc, which is still technically locked down because of the Mutex. Once 
+    // this is done, the lock is lifted, and the inner clone is left behind as a read-only 
+    // reference for the Camera and all associated types.
+    mgr: Arc<Mutex<Arc<Mgr>>>,
+}
+
+impl CameraManager {
+    pub fn new() -> Option<CameraManager> {
+        if unsafe {mgr_instanced.load(Ordering::Acquire)} {
+            return None;
+        }
+
+        let res = Some(CameraManager {
+            mgr: Arc::new(Mutex::new(Arc::new(Mgr(mgr::CameraManager::new().ok()?))))
+        });
+        unsafe {
+            mgr_instanced.store(true, Ordering::Release);
+        }
+
+        res
+    }
+
+    pub fn count(&self) -> usize {
+        self.mgr.lock().unwrap().0.cameras().len()
+    }
+
+    fn instance(&self) -> Arc<Mutex<Arc<Mgr>>> {
+        self.mgr.clone()
+    }
+
+}
+
+impl Drop for CameraManager {
+    fn drop(&mut self) {
+        unsafe {
+            mgr_instanced.store(false, Ordering::Release);
+        }
+    }
+}
+
+
+enum Control {
+    Start,
+    Stop,
+    Shutdown,
+}
 
 pub struct Camera {
-    _camref: &'static camera::Camera<'static>,
-    cam: Arc<Mutex<Cam>>,
-    cfgs: camera::CameraConfiguration,
-    // reqs: Vec<Request>,
-
-
-    frame_size: (usize, usize),
-    frame_id: Arc<AtomicU64>,
-    // frame_tx: mpsc::Sender<Request>,
-    // frame_rx: mpsc::Reciever<Request>,
     last_frame: Arc<RwLock<Frame>>,
-    format: PixelFormat,
-
-    worker: Option<WorkerThread<Request>>,
+    frame_id: Arc<AtomicU64>,
+    worker: JoinHandle<()>,
+    ctl: mpsc::Sender<Control>,
+    size: (usize, usize),
 }
 
 impl Camera {
-
-    fn alloc_requests(&mut self) -> Vec<Request> {
-        let mut alloc = FrameBufferAllocator::new(&self.cam.lock().unwrap());
-
-        let cfg = self.cfgs.get(0).unwrap();
-        let stream = cfg.stream().unwrap();
-        let buffers = alloc.alloc(&stream).unwrap();
-
-        buffers.into_iter().map(|buf| {
-            let buf = MemoryMappedFrameBuffer::new(buf).unwrap();
-            let mut req = self.cam.lock().unwrap().create_request(None).unwrap();
-            req.add_buffer(&stream, buf).unwrap();
-            req
-        }).collect::<Vec<_>>()
-    }
-
-    fn make_worker(&mut self) -> WorkerThread<Request> {
-        let thread_cam = self.cam.clone();
-        let thread_last_frame = self.last_frame.clone();
-        let thread_frame_id = self.frame_id.clone();
-        let cfg = self.cfgs.get(0).unwrap();
-        
-        let stream = cfg.stream().unwrap();
-        let width = cfg.get_size().width as usize;
-        let height = cfg.get_size().height as usize;
-
-        let worker = WorkerThread::new(move |rx| {
-            let mut req: Request = rx.recv()?;
-            let frame_buf: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
-            let planes = frame_buf.data();
-            let frame_data = planes.get(0).unwrap();
-
-            let frame_len = frame_buf.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize;
-
-            let frame = Frame::new(width, height, &frame_data[..frame_len]);
-            
-            *thread_last_frame.write().unwrap() = frame;
-            thread_frame_id.fetch_add(1, Ordering::AcqRel);
-
-            req.reuse(ReuseFlag::REUSE_BUFFERS);
-            thread_cam.lock().unwrap().queue_request(req)?;
-            Ok(())
-        });
-        worker
-    }
-
-    fn from_config(srcam: &'static camera::Camera<'static>,
-                   format: PixelFormat,
-                   // mut cam: camera::ActiveCamera<'static>,
-                   mut cfgs: camera::CameraConfiguration) -> Option<Camera> {
-        let mut cam = srcam.acquire().ok()?;
-        match cfgs.validate() {
-            CameraConfigurationStatus::Valid => Some(()),
-            CameraConfigurationStatus::Adjusted => {
-                if cfgs.get(0).unwrap().get_pixel_format() != format.libcamera_pixel_format() {
-                    None
-                } else {
-                    Some(())
-                }
-            }
-            CameraConfigurationStatus::Invalid => None,
-        }?;
-
-        cam.configure(&mut cfgs).ok()?;
-        let cfg = cfgs.get(0).unwrap();
-        // let stream = cfg.stream().unwrap();
-
-        // let (tx, rx) = mpsc::channel();
- // 
-        // cam.on_request_completed(move |req| {
-        //     tx.send(req).unwrap();
-        // });
-
-        let width = cfg.get_size().width as usize;
-        let height = cfg.get_size().height as usize;
-
-        let last_frame = Arc::new(RwLock::new(Frame::blank(width, height)));
-
-        let cam = Cam(cam);
-        let cam = Arc::new(Mutex::new(cam));
-
-        // let thread_cam = cam.clone();
-        // let thread_last_frame = last_frame.clone();
-        // 
-        let frame_id = Arc::new(AtomicU64::new(0));
-        // let thread_frame_id = frame_id.clone();
-        // 
-        // let worker = SignaledThread::new(move || {
-        //     let mut req = rx.recv()?;
-        //     let frame_buf: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
-        //     let planes = frame_buf.data();
-        //     let frame_data = planes.get(0).unwrap();
-        // 
-        //     let frame_len = frame_buf.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize;
-        // 
-        //     let frame = Frame::new(width, height, &frame_data[..frame_len]);
-        //     
-        //     *thread_last_frame.write().unwrap() = frame;
-        //     thread_frame_id.fetch_add(1, Ordering::AcqRel);
-        // 
-        //     req.reuse(ReuseFlag::REUSE_BUFFERS);
-        //     thread_cam.lock().unwrap().queue_request(req)?;
-        //     Ok(())
-        // });
-
-        Some(Camera {
-            _camref: srcam,
-            cam,
-            cfgs,
-            frame_size: (width, height),
-            frame_id,
-            last_frame,
-            format,
-            worker: None
-        })
-    }
-
-    pub fn new(cam: &'static camera::Camera<'static>, format: PixelFormat) -> Option<Camera> {
+    pub fn new(cam_index: usize, mgr: &CameraManager, format: PixelFormat) -> Option<Camera> {
+        let _mgr = mgr.mgr.lock().unwrap();
+        let _cameras = _mgr.0.cameras();
+        let cam = _cameras.get(cam_index).unwrap();
+        println!("  getting cam");
         let mut cfgs = cam.generate_configuration(&[StreamRole::VideoRecording]).unwrap();
         let mut cfg = cfgs.get_mut(0).unwrap();
         cfg.set_pixel_format(format.libcamera_pixel_format());
         cfg.set_buffer_count(4);
-        Camera::from_config(cam, format, cfgs)
+        println!("  set config");
+        Camera::from_config(mgr, cam_index, format, cfgs)
     }
 
-    pub fn with_size(cam: &'static camera::Camera<'static>, 
+    pub fn with_size(cam_index:usize, mgr: &CameraManager, 
                      width: usize, height: usize,
                      format: PixelFormat) -> Option<Camera> {
+        let _mgr = mgr.mgr.lock().unwrap();
+        let _cameras = _mgr.0.cameras();
+        let cam = _cameras.get(cam_index).unwrap();
         let mut cfgs = cam.generate_configuration(&[StreamRole::VideoRecording]).unwrap();
         let mut cfg = cfgs.get_mut(0).unwrap();
         cfg.set_pixel_format(format.libcamera_pixel_format());
         cfg.set_buffer_count(4);
         cfg.set_size(Size{width: width as u32, height: height as u32});
-        Camera::from_config(cam, format, cfgs)
+        Camera::from_config(mgr, cam_index, format, cfgs)
+    }
+    fn from_config(mgr: &CameraManager, cam_index: usize, 
+                   format:PixelFormat, 
+                   mut cfgs: camera::CameraConfiguration) -> Option<Camera> {
+
+        if cam_index >= mgr.count() {
+            return None;
+        }
+        let cfgs = Cfg(cfgs);
+
+        println!("  getting mgr copy");
+        let mgr = mgr.instance();
+
+        let cfg = cfgs.0.get(0).unwrap();
+        let size = cfg.get_size();
+        println!("  setting up thread vars");
+        let (ctl_tx, ctl_rx) = mpsc::channel();
+        let last_frame = Arc::new(RwLock::new(Frame::blank(size.width as usize, size.height as usize)));
+        let frame_id = Arc::new(AtomicU64::new(0));
+
+        let thread_last_frame = last_frame.clone();
+        let thread_frame_id = frame_id.clone();
+
+
+        let thread_builder = thread::Builder::new();
+        let worker = thread_builder.spawn(move || {
+            let mut cfgs = cfgs;
+            let mgr_lock = mgr.lock().unwrap();
+            let mgr = mgr_lock.clone();
+            let cameras = mgr.0.cameras();
+            let srccam = cameras.get(cam_index).unwrap();
+
+            match cfgs.0.validate() {
+                CameraConfigurationStatus::Valid => Some(()),
+                CameraConfigurationStatus::Adjusted => {
+                    if cfgs.0.get(0).unwrap().get_pixel_format() != format.libcamera_pixel_format() {
+                        None
+                    } else {
+                        Some(())
+                    }
+                }
+                CameraConfigurationStatus::Invalid => None,
+            }.unwrap();
+            // let srccam = Cam(srccam);
+            
+            let mut cam = srccam.acquire().unwrap();
+            cam.configure(&mut cfgs.0).unwrap();
+            let cfg = cfgs.0.get(0).unwrap();
+            let stream = cfg.stream().unwrap();
+           
+            
+            drop(mgr_lock);
+
+            let (tx, rx) = mpsc::channel();
+            cam.on_request_completed(move |req| {
+                tx.send(req).unwrap();
+            });
+            
+            let mut term_flag = false;
+            let mut running = false;
+            while !term_flag {
+                match ctl_rx.try_recv() {
+                    Ok(msg) => match msg {
+                        Control::Start => {
+                            let mut alloc = FrameBufferAllocator::new(&cam);
+
+                            let buffers = alloc.alloc(&stream).unwrap();
+                            let reqs = buffers.into_iter().map(|buf| {
+                                let buf = MemoryMappedFrameBuffer::new(buf).unwrap();
+                                let mut req = cam.create_request(None).unwrap();
+                                req.add_buffer(&stream, buf).unwrap();
+                                req
+                            }).collect::<Vec<_>>();
+
+
+
+                            cam.start(None).unwrap();
+                            // Submit requests here because there isn't really a good place to do that otherwise.
+                            for req in reqs {
+                                cam.queue_request(req).unwrap();
+                            }
+                            running = true;
+                        }
+                        Control::Stop => {
+                            // stop camera stream
+                            running = false;
+                            cam.stop();
+                            while rx.try_recv().is_ok() {
+                                // drain the channel
+                                // nothing doing here
+                            }
+                        }
+                        Control::Shutdown => {
+                            if running {
+                                running = false;
+                                cam.stop();
+                                term_flag = true;
+                            }
+                        }
+                    }
+                    Err(mpsc::TryRecvError::Empty) => {/* do nothing */}
+                    Err(mpsc::TryRecvError::Disconnected) => {
+                        // Do not set term_flag, since shutting down the control channel means
+                        // this thread is no longer reachable. That is defined, but breaking
+                        // behaviour.
+                        break;
+                    } 
+                }
+                if !running {
+                    thread::sleep(std::time::Duration::from_millis(10));
+                    continue;
+                }
+
+                // do camera shenanigans
+                match rx.try_recv() {
+                    Ok(mut req) => {
+                        // do processing;
+                        let frame_buf: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
+                        let planes = frame_buf.data();
+                        let frame_data = planes.get(0).unwrap();
+
+                        let frame_len = frame_buf.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize;
+                        let frame = Frame::new(size.width as usize, size.height as usize, &frame_data[..frame_len]);
+                        *thread_last_frame.write().unwrap() = frame;
+
+                        thread_frame_id.fetch_add(1, Ordering::AcqRel);
+
+                        req.reuse(ReuseFlag::REUSE_BUFFERS);
+                        cam.queue_request(req).unwrap();
+                    }
+                    Err(mpsc::TryRecvError::Empty) => {continue;} // basically no-op
+                    Err(mpsc::TryRecvError::Disconnected) => {break;} // something happened
+                }
+            }
+        });
+
+        let worker = worker.ok()?;
+
+        Some(Camera {
+            last_frame,
+            frame_id,
+            worker,
+            ctl: ctl_tx,
+            size: (size.width as usize, size.height as usize)
+        })
     }
 }
 
@@ -283,56 +366,78 @@ impl FrameSource for Camera {
     fn get_frame(&self) -> Frame {
         self.last_frame.read().unwrap().clone()
     }
-
     fn start(&mut self) -> Result<(), anyhow::Error> {
-        if self.worker.is_some() {
-            // FIXME
-            // TODO
-            // FIXME
-            panic!("Figure this out later") 
-        }
-        self.cam.lock().unwrap().start(None).unwrap();
-       
-        let reqs = self.alloc_requests(); 
-        let worker = self.make_worker();
-        self.worker = Some(worker);
-        
-        let tx = self.worker.as_ref().unwrap().sender();
-        self.worker.as_mut().unwrap().start()?;
-      
-        self.cam.lock().unwrap().on_request_completed(move |req| {
-            tx.send(req).unwrap();
-        });
-
-        for req in reqs {
-            self.cam.lock().unwrap().queue_request(req).unwrap();
-        }
+        self.ctl.send(Control::Start)?;
         Ok(())
     }
 
     fn stop(&mut self) -> Result<(), anyhow::Error> {
-        if self.worker.is_none() {
-            // FIXME
-            // TODO
-            // FIXME
-            panic!("Figure this out later") 
-        }
-        let mut worker = self.worker.take().unwrap();
-        worker.kill();
-        worker.join()?;
-
-        self.cam.lock().unwrap().stop()?;
+        self.ctl.send(Control::Stop)?;
         Ok(())
     }
 
     fn frame_size(&self) -> (usize, usize) {
-        self.frame_size
+        self.size
     }
 
     fn frame_id(&self) -> u64 {
         self.frame_id.load(Ordering::Acquire)
     }
 }
+// impl FrameSource for Camera {
+//     fn get_frame(&self) -> Frame {
+//         self.last_frame.read().unwrap().clone()
+//     }
+// 
+//     fn start(&mut self) -> Result<(), anyhow::Error> {
+//         if self.worker.is_some() {
+//             // FIXME
+//             // TODO
+//             // FIXME
+//             panic!("Figure this out later") 
+//         }
+//         self.cam.lock().unwrap().start(None).unwrap();
+//        
+//         let reqs = self.alloc_requests(); 
+//         let worker = self.make_worker();
+//         self.worker = Some(worker);
+//         
+//         let tx = self.worker.as_ref().unwrap().sender();
+//         self.worker.as_mut().unwrap().start()?;
+//       
+//         self.cam.lock().unwrap().on_request_completed(move |req| {
+//             tx.send(req).unwrap();
+//         });
+// 
+//         for req in reqs {
+//             self.cam.lock().unwrap().queue_request(req).unwrap();
+//         }
+//         Ok(())
+//     }
+// 
+//     fn stop(&mut self) -> Result<(), anyhow::Error> {
+//         if self.worker.is_none() {
+//             // FIXME
+//             // TODO
+//             // FIXME
+//             panic!("Figure this out later") 
+//         }
+//         let mut worker = self.worker.take().unwrap();
+//         worker.kill();
+//         worker.join()?;
+// 
+//         self.cam.lock().unwrap().stop()?;
+//         Ok(())
+//     }
+// 
+//     fn frame_size(&self) -> (usize, usize) {
+//         self.frame_size
+//     }
+// 
+//     fn frame_id(&self) -> u64 {
+//         self.frame_id.load(Ordering::Acquire)
+//     }
+// }
 
 pub unsafe fn enable_libcamera_logging(enable: bool) {
     if enable {
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,28 @@ mod camera;
 mod signal_thread;
 mod thread;
 
-use std::cell::OnceCell;
-use libcamera::camera_manager::CameraManager;
-use camera::{Camera, PixelFormat, FrameSource};
+// use libcamera::camera_manager::CameraManager;
+use camera::{Camera, PixelFormat, FrameSource, CameraManager};
 
-lazy_static::lazy_static! {
-    static ref mgr: CameraManager = CameraManager::new().unwrap();
-}
 
 fn main() {
 
-    println!("Acquiring basic camera");
-    let cameras = mgr.cameras();
-    let cam = cameras.get(0).unwrap();
+    println!("Creating manager");
+    let mgr = CameraManager::new().unwrap();
 
     println!("creating wrapper");
-    let mut cam = Camera::new(&cam, PixelFormat::MJPEG).unwrap();
+    let mut cam = Camera::new(0, &mgr, PixelFormat::MJPEG).unwrap();
 
+    // println!("Acquiring basic camera");
+    // let cameras = mgr.cameras();
+    // let cam = cameras.get(0).unwrap();
+    // 
+    // println!("creating wrapper");
+    // let mut cam = Camera::new(&cam, PixelFormat::MJPEG).unwrap();
+    
     println!("starting background capture");
     cam.start().unwrap();
-
+    
     println!("getting frame");
     let frame = cam.get_frame();
     println!("frame get!");
